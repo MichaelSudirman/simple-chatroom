178b6b51d5dabb9c5e0773ea88fe58ca29ccd0a6
MichaelSudirman <astradon.michael@gmail.com>
2020-03-07 21:22:01 +1000

    added chatroom feature using chatroom

---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var socket = require("socket.io");
+
+// App setup
+var app = express();
+var server = app.listen(4000, function() {
+  console.log("listening to requests on port 4000");
+});
+
+// Serve static files under public folder
+app.use(express.static("public"));
+
+app.get("/hello", function(req, res) {
+  res.send("<h1>Hello</h1>");
+});
+
+// Socket setup
+var io = socket(server);
+
+io.on("connection", function(socket) {
+  console.log("made socket connection", socket.id);
+
+  // Handle chat event
+  socket.on("chat", function(data) {
+    io.sockets.emit("chat", data);
+  });
+
+  socket.on("typing", function(data) {
+    socket.broadcast.emit("typing", data);
+  });
+});
